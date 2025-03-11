b61e277d72f0204087dcaf16719001ea903633b3
sangledev95 <lqsang19952@gmail.com>
2025-03-11 20:33:26 +0700

    Update

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { WebhookService } from './webhook.service';
 import { WebhookController } from './webhook.controller';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'static'), // Chỉ phục vụ file trong module webhook
+      serveRoot: '/webhook', // Truy cập qua /webhook/xyz.html
+    }),
+  ],
   providers: [WebhookService],
-  controllers: [WebhookController]
+  controllers: [WebhookController],
 })
 export class WebhookModule {}
