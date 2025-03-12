5e967991cb9a0d518f1826324f1478bf47627e23
sangledev95 <lqsang19952@gmail.com>
2025-03-12 21:38:44 +0700

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,11 @@ module.exports = {
         name: "webhook-api",
         script: "npm",
         args: "run start",
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: "500M",
+        cwd: "C:/lnt/lnt-webhook-api", // Thay bằng đường dẫn đúng đến thư mục dự án của bạn
+        interpreter: "none", // Quan trọng: Không sử dụng Node.js để chạy npm
         env: {
           NODE_ENV: "production"
         }
       }
     ]
   };
-  
