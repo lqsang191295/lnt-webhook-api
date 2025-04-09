2beadfe27f1b4ebcc0b829c8845cdb65fb1d0f20
sangledev95 <lqsang19952@gmail.com>
2025-04-09 21:29:57 +0700

    Update

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Sse } from '@nestjs/common';
-import { Observable, map } from 'rxjs';
-import { dbChangeSubject } from 'src/event-stream';
+// src/notification/notification.controller.ts
+import {
+  Controller,
+  Get,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { dbChangeSubject } from 'src/event-stream'; // cái subject bạn tạo
+import { map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 interface ServerSentEvent<T = any> {
   data: T;
@@ -11,8 +19,22 @@ interface ServerSentEvent<T = any> {
 
 @Controller('notification')
 export class NotificationController {
-  @Sse('sse')
-  sendNotifications(): Observable<ServerSentEvent> {
+  @Get('sse')
+  sendNotifications(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ): Observable<ServerSentEvent> {
+    const origin = req.headers.origin || '*';
+
+    res.set({
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Credentials': 'true',
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive',
+      'X-Accel-Buffering': 'no',
+    });
+
     return dbChangeSubject.pipe(map((data) => ({ data })));
   }
 }
