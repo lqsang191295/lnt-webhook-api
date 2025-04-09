1886110cfb714bca181a42ce9e41a32d8779a99d
sangledev95 <lqsang19952@gmail.com>
2025-04-09 21:53:19 +0700

    Update

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Sse } from '@nestjs/common';
+import { Controller, Sse, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { Observable, map } from 'rxjs';
 import { Public } from 'src/common/decorators/public.decorator';
 import { dbChangeSubject } from 'src/event-stream';
@@ -14,7 +15,12 @@ interface ServerSentEvent<T = any> {
 export class NotificationController {
   @Public()
   @Sse('sse')
-  sendNotifications(): Observable<ServerSentEvent> {
+  sendNotifications(@Res() res: Response): Observable<ServerSentEvent> {
+    res.setHeader('Access-Control-Allow-Origin', 'http://172.16.0.10:3004'); // 👈 Thêm dòng này
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
     return dbChangeSubject.pipe(map((data) => ({ data })));
   }
 }
